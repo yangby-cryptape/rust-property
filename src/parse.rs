1d99093c39a8a9eeeb10930622078cdbb70bdb66
Boyu Yang <yangby@cryptape.com>
2020-03-30 23:45:15 +0800

    chore: update the years in copyright notice

--- src/parse.rs
+++ src/parse.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 Boyu Yang
+// Copyright (C) 2019-2020 Boyu Yang
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
